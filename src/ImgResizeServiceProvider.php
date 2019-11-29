cf6ad09e4f03a29fdcc10953cfa659e3c4a2231e
Martua Nasution <marithonst@gmail.com>
2019-11-29 18:14:37 +0700

    initial projeck

---
+++ src/ImgResizeServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+namespace Devnas\Imgresize;
+use Illuminate\Support\ServiceProvider;
+
+class ImgResizeServiceProvider extends ServiceProvider {
+
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
+?>
